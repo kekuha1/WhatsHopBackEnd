89d6e7ae6cfd0ab5937fdfe2dcd650d63b3a1dfb
kekuha1 <109771410+kekuha1@users.noreply.github.com>
2023-03-25 16:10:32 -0400

    brewrouter and app use for it

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,9 @@
-
 import * as functions from "firebase-functions";
 import express, { Application } from "express";
 import cors from "cors";
 import userRouter from "./routes/userRouter";
+import reviewsRouter from "./routes/reviewsRouter";
+import breweriesRouter from "./routes/breweriesRouter";
 
 const app: Application = express();
 
@@ -10,5 +11,7 @@ app.use(cors());
 app.use(express.json());
 // Define your endpoint and specify the HTTP method
 app.use("/", userRouter);
+app.use("/review", reviewsRouter);
+app.use("/breweries", breweriesRouter);
 
 export const api = functions.https.onRequest(app);
---
+++ functions/src/routes/breweriesRouter.ts
@@ -0,0 +1,103 @@
+import express from 'express';
+import { getClient } from '../db';
+import { ObjectId } from 'mongodb';
+import Brewery from '../model/Brewery';
+
+const breweriesRouter = express.Router();
+
+const errorResponse = (error: any, res: any) => {
+  console.error('FAIL', error);
+  res.status(500).json({ message: 'Internal Server Error' });
+};
+breweriesRouter.get('/breweries', async (req, res) => {
+  try {
+     const client = await getClient();
+     const cursor = client.db().collection<Brewery[]>('breweries').find();
+     const results = await cursor.toArray();
+     results ? res.status(200).json(results):res.status(404).json({message: 'Not found'});
+  } catch (err) {
+    errorResponse(err, res);
+  }
+});
+
+breweriesRouter.get('/breweries/:id', async (req, res) => {
+  try {
+let id:string = req.params.id as string;
+     const client = await getClient();
+     const results = client.db().collection<Brewery>('breweries').find({_id: new ObjectId(id)});
+if (results){
+     res.status(200).json(results);
+} else {
+res.status(404).json({message: 'ID not Found'});
+}
+  } catch (err) {
+    errorResponse(err, res);
+  }
+});
+
+breweriesRouter.post('/breweries', async (req, res) => {
+  try {
+     const client = await getClient();
+     const newItem = req.body
+    client.db().collection<Brewery>('breweries').insertOne(newItem);
+     res.status(200);
+     res.json(newItem);
+  } catch (error) {
+    errorResponse(error, res);
+  }
+});
+
+breweriesRouter.put('/breweries/:id', async (req, res) => {
+  try {
+let id: string = req.params.id as string;
+const replacement = req.body;
+replacement._id = new ObjectId(id);
+const client = await getClient();
+const result = await client.db().collection<Brewery>('breweries').replaceOne({_id: new ObjectId(id)}, replacement);
+if (result.modifiedCount) {
+  res.status(200);
+  res.json(replacement);
+} else {
+  res.status(404);
+  res.send('ID not found');
+}
+  } catch (error) {
+    errorResponse(error, res);
+  }
+});
+
+breweriesRouter.patch('/breweries/:id', async (req, res) => {
+  try {
+let id: string = req.params.id as string;
+const update = req.body;
+const client = await getClient();
+const result = await client.db().collection<Brewery>('breweries').updateOne({_id: new ObjectId(id)}, {$set: update});
+if (result.modifiedCount) {
+  res.status(200);
+  res.json(update);
+} else {
+  res.status(404);
+  res.send('ID not found');
+}
+  } catch (error) {
+    errorResponse(error, res);
+  }
+});
+
+breweriesRouter.delete('/breweries/:id', async (req, res) => {
+  try {
+let id: string = req.params.id as string;
+const client = await getClient();
+const result = await client.db().collection<Brewery>('breweries').deleteOne({_id: new ObjectId(id)});
+if (result.deletedCount) {
+  res.sendStatus(204);
+} else {
+  res.status(404);
+  res.send('No ID found');
+}
+  } catch (error) {
+    errorResponse(error, res);
+  }
+});
+
+export default breweriesRouter;
