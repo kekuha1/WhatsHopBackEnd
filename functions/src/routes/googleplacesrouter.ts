e02cc92952a06334a4ea469e27f5327a6781a6b6
David <isaacsdavid28@gmail.com>
2023-06-20 20:52:15 -0400

    google places

--- functions/src/routes/googleplacesrouter.ts
+++ functions/src/routes/googleplacesrouter.ts
@@ -1,22 +1,21 @@
-const express = require('express');
-const axios = require('axios');
-
+import axios from "axios";
+import express from "express";
 
 const googleplacesrouter = express.Router();
 
-googleplacesrouter.post('/', async (req: any, res: any) => {
+googleplacesrouter.get('/', async (req, res) => {
   try {
-    const { name, city, state } = req.body;
+    const { name, city, state } = req.query;
     const formattedAddress = `${name} ${city} ${state}`;
     const encodedAddress = encodeURIComponent(formattedAddress);
     const apiKey = process.env.GOOGLE_PLACES_API_KEY;
     const url = `https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=${encodedAddress}&inputtype=textquery&fields=formatted_address,name,rating,opening_hours,geometry,photo&key=${apiKey}`;
 
     const response = await axios.get(url);
-    res.send(response.data);
+    res.json(response.data);
   } catch (error) {
     console.log(error);
-    res.status(500).send('Error occurred');
+    res.status(500).json({ error: 'Error occurred' });
   }
 });
 
